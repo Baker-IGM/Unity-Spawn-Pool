8f2cdc300cf36550228048aa2785a976fd198493
Prof Baker <eabigm@rit.edu>
2020-02-26 10:50:52 -0500

    Updated to 2019
    Switched to Canvas
    Switched to new Input System

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(RectTransform))]
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+    RectTransform rect;
+    
     const string k_FIRE_AXIS = "Fire1";
 
     [SerializeField]
@@ -12,29 +17,48 @@ public class Player : MonoBehaviour
 
     Transform NewBullet;
 
+    [SerializeField]
+    float moveScalar = 1;
+    [SerializeField]
+    Vector2 moveDelta;
+
 	// Use this for initialization
-	void Start () {
-        //Debug.Log(transform.forward);
+	void Start ()
+    {
+        rect = GetComponent<RectTransform>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
         #region Firing
-        if (fireTimer > 0)
-        {
-            fireTimer -= Time.deltaTime;
-        }
+        //      if (fireTimer > 0)
+        //      {
+        //          fireTimer -= Time.deltaTime;
+        //      }
 
-		if(fireTimer <= 0 && Input.GetAxis(k_FIRE_AXIS) > 0)
-        {
-            BulletManager.Instance.GetBullet().Fire(transform.position, transform.rotation);
+        //if(fireTimer <= 0 && Input.GetAxis(k_FIRE_AXIS) > 0)
+        //      {
+        //          BulletManager.Instance.GetBullet().Fire(transform.position, transform.rotation);
 
-            fireTimer = fireCoolDown;
-        }
+        //          fireTimer = fireCoolDown;
+        //      }
         #endregion
 
-        Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.rotation = Quaternion.LookRotation(mouseWorldPos, -Vector3.forward);
+        //Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //transform.rotation = Quaternion.LookRotation(mouseWorldPos, -Vector3.forward);
+
+        rect.anchoredPosition += moveDelta;
+    }
+
+    public void OnMove(InputValue value)
+    {
+        moveDelta = value.Get<Vector2>();
+
+        moveDelta *= moveScalar;
+
+        rect.anchoredPosition += moveDelta;
+
+        //transform.Translate()
     }
 }
