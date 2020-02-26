dc764e3aefcbb09747e5f87ae2c9a8ad0140db51
Eric Baker <eabigm@rit.edu>
2020-02-26 16:07:58 -0500

    Added look at code

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,15 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(RectTransform))]
 public class Player : MonoBehaviour
 {
-    [SerializeField]
-    RectTransform rect;
+    RectTransform rect, parentRect;
+
+    #region Look at vars
     [SerializeField]
     Vector2 position;
-    
+
+    [SerializeField]
+    float angle;
+
+    [SerializeField]
+    Vector2 lookAtVect;
+
+    [SerializeField]
+    Vector2 lookAtDelta;
+    #endregion
+
+    #region Firing vars
     const string k_FIRE_AXIS = "Fire1";
 
     [SerializeField]
@@ -18,14 +31,12 @@ public class Player : MonoBehaviour
     float fireTimer;
     
     Transform NewBullet;
+    #endregion
 
-    [SerializeField]
-    Vector2 lookAtVect;
-
-    #region Movement
+    #region Movement vars
     [SerializeField]
     float moveScalar = 1;
-    [SerializeField]
+
     Vector2 moveDelta;
     #endregion
 
@@ -33,6 +44,10 @@ public class Player : MonoBehaviour
     void Start ()
     {
         rect = GetComponent<RectTransform>();
+
+        parentRect = transform.parent.GetComponent<RectTransform>();
+
+        //MovePlayer(parentRect.rect.size / 2f);
 	}
 	
 	// Update is called once per frame
@@ -55,15 +70,51 @@ public class Player : MonoBehaviour
         //
         //      Player movement
         //
-        rect.anchoredPosition += moveDelta;
-        position = rect.anchoredPosition;
+        MovePlayer(moveDelta);
 
         //
         //      Rotate to aim at the pointer
         //
-        rect.LookAt(Camera.main.ScreenToWorldPoint(lookAtVect));
+        lookAtDelta = position - lookAtVect;
+
+        
+
+        if (lookAtDelta.y < 0)
+        {
+            //      1
+            if(lookAtDelta.x < 0)
+            {
+                angle = Mathf.Atan(lookAtDelta.y / lookAtDelta.x);
+            }
+            //      2
+            else
+            {
+                angle = Mathf.Atan(lookAtDelta.y / lookAtDelta.x) + Mathf.PI;
+            }
+        }
+        else
+        {
+            //      3
+            if (lookAtDelta.x >= 0)
+            {
+                angle = Mathf.Atan(lookAtDelta.y / lookAtDelta.x) + Mathf.PI;
+            }
+            //      4
+            else
+            {
+                angle = Mathf.Atan(lookAtDelta.y / lookAtDelta.x) + (Mathf.PI * 2f);
+            }
+        }
+
+        if (!float.IsNaN(angle))
+        {
+            angle *= Mathf.Rad2Deg;
+
+            transform.rotation = Quaternion.Euler(0, 0, angle - 90f);
+        }
     }
 
+    #region Movment Logic
     public void OnMove(InputValue value)
     {
         moveDelta = value.Get<Vector2>();
@@ -73,8 +124,16 @@ public class Player : MonoBehaviour
         rect.anchoredPosition += moveDelta;
     }
 
+    void MovePlayer(Vector2 delta)
+    {
+        rect.anchoredPosition += delta;
+
+        position = rect.anchoredPosition;
+    }
+    #endregion
+
     public void OnLook(InputValue value)
     {
-        lookAtVect = value.Get<Vector2>();
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(parentRect, value.Get<Vector2>(), Camera.main, out lookAtVect);
     }
 }
