89671f3efe237af7fdb37bc31587d1fb425d665b
Eric Baker <eabigm@rit.edu>
2020-03-03 10:02:37 -0500

    Created scriptableObject for bullet data
    organized scripts

---
+++ Assets/Scripts/BulletManagerScriptableObejct.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/BulletManagerScriptableObject", order = 1)]
+public class BulletManagerScriptableObejct : ScriptableObject
+{
+    public int startingBulletCount;
+
+    public string bulletName = "Bullet_{0:d4}";
+
+    public Color playerBulletColor;
+
+    public Color enemyBulletColor;
+}
